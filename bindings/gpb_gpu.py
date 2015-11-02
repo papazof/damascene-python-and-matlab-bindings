6870e58f8a8b6b07bd2ed1a8207cdd06292baac7
papazof <papazof@gmail.com>
2015-11-02 14:12:53 +0200

    Fix error in finding include dir in gpb_gpu.py and remove unused libs.

--- bindings/gpb_gpu.py
+++ bindings/gpb_gpu.py
@@ -3,14 +3,13 @@ import numpy as np
 import os
 from scipy.weave import inline,converters
 
-cuda_drv_path="/usr/lib/nvidia-352"
-#cuda_sdk_path="/usr/local/cuda/C/lib"
 cuda_path="/usr/local/cuda/lib64"
 acml_path="/usr/local/acml/gfortran64/lib"
-damascene_path=os.path.abspath(os.pardir)+"/build"
+cur_dir=os.path.abspath(os.path.dirname(__file__))
+damascene_path=os.path.join(cur_dir, '../build')
 
 libraries="cuda cudart cublas acml damascene".split()
-library_dirs=[cuda_path,cuda_drv_path,acml_path,damascene_path]
+library_dirs=[cuda_path,acml_path,damascene_path]
 
 def gpb(image):
     [height, width]=image.shape[:2]
@@ -27,7 +26,7 @@ def gpb(image):
     code="""
          gpb((float*)&padded_image(0,0),width,height,&borders(0,0),&textons(0,0),&orientations(0,0));
          """
-    inline(code,['padded_image','textons','width', 'height','borders','orientations'], type_converters=converters.blitz, compiler = 'gcc',headers=['"'+os.path.abspath('gpb.h')+'"'],include_dirs=['.'],libraries=libraries,library_dirs=library_dirs,runtime_library_dirs=library_dirs,verbose=0,force=1)
+    inline(code,['padded_image','textons','width', 'height','borders','orientations'], type_converters=converters.blitz, compiler = 'gcc',headers=['"'+os.path.join(cur_dir, 'gpb.h')+'"'],include_dirs=['.'],libraries=libraries,library_dirs=library_dirs,runtime_library_dirs=library_dirs,verbose=0,force=1)
     orientations=orientations.T
     return [borders,textons,orientations]
 
@@ -35,7 +34,7 @@ def gpb(image):
 if __name__ == "__main__":
     from PIL import Image
     import matplotlib.pyplot as plt
-
+    
     #image=Image.open('../damascene/polynesia.ppm');
     image=Image.open('./100007.png')
     data=np.array(image.getdata()).reshape(image.size[1],image.size[0],1)
