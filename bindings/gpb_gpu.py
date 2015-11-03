92e7ad06ea561f1eeb47332f5a8565f1c3ecbef0
papazof <papazof@gmail.com>
2015-11-03 17:10:59 +0200

    Disable SciPy generated warnings in gpb_gpu.py.

--- bindings/gpb_gpu.py
+++ bindings/gpb_gpu.py
@@ -3,13 +3,19 @@ import numpy as np
 import os
 from scipy.weave import inline,converters
 
+cur_dir=os.path.abspath(os.path.dirname(__file__))
+
+# Set library dirs here
 cuda_path="/usr/local/cuda/lib64"
 acml_path="/usr/local/acml/gfortran64/lib"
-cur_dir=os.path.abspath(os.path.dirname(__file__))
 damascene_path=os.path.join(cur_dir, '../build')
 
 libraries="cuda cudart cublas acml damascene".split()
 library_dirs=[cuda_path,acml_path,damascene_path]
+
+# Set compiler options
+compiler='gcc'
+extra_cmp_args=['-Wno-cpp',' -Wno-parentheses'] # disable generated NumPy warnings
 
 def gpb(image):
     [height, width]=image.shape[:2]
@@ -26,7 +32,11 @@ def gpb(image):
     code="""
          gpb((float*)&padded_image(0,0),width,height,&borders(0,0),&textons(0,0),&orientations(0,0));
          """
-    inline(code,['padded_image','textons','width', 'height','borders','orientations'], type_converters=converters.blitz, compiler = 'gcc',headers=['"'+os.path.join(cur_dir, 'gpb.h')+'"'],include_dirs=['.'],libraries=libraries,library_dirs=library_dirs,runtime_library_dirs=library_dirs,verbose=0,force=1)
+    inline(code, ['padded_image','textons','width', 'height','borders','orientations'], type_converters=converters.blitz,
+           compiler=compiler, extra_compile_args=extra_cmp_args,
+           headers=['"'+os.path.join(cur_dir, 'gpb.h')+'"'], include_dirs=['.'],
+           libraries=libraries, library_dirs=library_dirs, runtime_library_dirs=library_dirs,
+           verbose=0, force=1)
     orientations=orientations.T
     return [borders,textons,orientations]
 
